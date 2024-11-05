871939395dc0ef944e5ee0fee1b9022df9e9e527
24LP8639 24LP8639 <24LP8639@G302-CC0120.local>
2024-11-05 19:13:14 +0800

    a

---
+++ exercises.js
@@ -0,0 +1,32 @@
+// function calculateProblems(hour) {
+//   totalProblems = hour * 2;
+//   console.log("Би", hour, "цаг суралцаад", totalProblems, "бодлого бодлоо");
+// }
+
+// calculateProblems(3);
+// calculateProblems(12);
+// calculateProblems(6);
+
+// // 5
+// function printFavoriteFood(name, food) {
+//   console.log(name, "loves to eat", food);
+// }
+// printFavoriteFood("Sara", "Pizza");
+
+// // 6
+// function printWeather(cityName, weather) {
+//   console.log(cityName, "хотод", weather, "байна");
+// }
+// printWeather("Ulaanbaatar", "-13C");
+// // 7
+// function printBookDetails(title, author, yearPublished) {
+//   console.log(title, author, yearPublished);
+// }
+// printBookDetails("Үзэхийн хязгаар", "Өлзийтөгс", "0000");
+
+// 8
+// function calculateArea(width, height) {
+//   const area = width, height(20, 30);
+//   console.log(calculateArea, + area);
+// }
+// calculateArea(20, 30)
--- star.js
+++ star.js
@@ -1,5 +1,5 @@
 // console.log(1);
-let n = 5;
+const n = 5;
 // let s = "";
 // for (let i = 0; i < n; i++) {
 //   for (let a = 0; a < n; a++) {
@@ -9,10 +9,104 @@ let n = 5;
 //   s = "";
 // }
 
-console.log(2);
-for (let i = 1, s = ""; i < 6; i++) {
-  s = s + "*";
-  console.log(s);
-}
+// console.log(2);
+let result = "";
+// for (let i = 1; i < 6; i++) {
+//   result = result + "* ";
+//   console.log(result);
+// }
 // console.log(3);
 // for (let i = 1; i <= n; ) {}
+// console.log(1);
+
+// let result = "";
+// for (k = 0; k < n; k++) {
+//   let i = 0;
+//   while (i < n) {
+//     result += "* ";
+//     i++;
+//   }
+//   result += "\n";
+// }
+
+// console.log(result);
+
+// console.log(2);
+// for (i = 0; i < n; i++) {
+//   let k = 0;
+//   while (k < i + 1) {
+//     result += "* ";
+//     k++;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// console.log(3);
+
+// for (k = 0; k < n; k++) {
+//   i = 0;
+//   for (j = 0; j < n - k - 1; j++) {
+//     result += " ";
+//   }
+//   while (i < k + 1) {
+//     i++;
+//     result += "* ";
+//   }
+//   result += "\n";
+// }
+
+// console.log(result);
+
+// console.log(3);
+// for (j = 0; j < n; j++) {
+//   let i = 0;
+//   for (k = 0; k < n - j - 1; k++) {
+//     result += " ";
+//   }
+//   while (i < n + 1) {
+//     result += "* ";
+//     i++;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// 4)
+
+//     *
+//    * *
+//   *   *
+//  *     *
+// * * * * *
+
+// console.log(4);
+// for (j = 0; j < n; j++) {
+//   let i = 0;
+//   for (k = 0; k < n - j - 1; k++) {
+//     result += " ";
+//   }
+//   while (i < j + 1) {
+//     result += "* ";
+//     i++;
+//   }
+//   result += "\n";
+// }
+
+// console.log(result);
+
+console.log(4);
+
+for (i = 0; i < n; i) {}
+
+// 5)
+
+//     *
+//    * *
+//   *   *
+//  *     *
+// *       *
+//  *     *
+//   *   *
+//    * *
+//     *
